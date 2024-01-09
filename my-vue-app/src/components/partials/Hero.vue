dd18e1d6d848f98f87579d8d07576eeffad9ced7
Anmol Rajbhandari <anmols@hotmail.com>
2024-01-09 14:54:11 +1100

    ex1 - banner

---
+++ my-vue-app/src/components/partials/Hero.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="ar-hero ar-flex-center">
+  <div class="text-white ar-flex-center flex-col container text-center">
+    <h1 class="text-5xl xl:text-8xl">Hello Developer!</h1>
+    <p class="text-lg mt-4 md:text-2xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+  </div>
+</div>
+</template>
